9f582d5e1f966a11ef1a159fe4e67d6e2081e8ec
sam bacha <sam@freighttrust.com>
2021-03-01 20:01:16 -0800

    v1.0.5

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /**
 * @file Prettier configuration for Conformance
-* @version 1.0.4
+* @version 1.0.5
 * @summary base config adapted from AirBNB to maximize performance
 */
 
 'use strict';
 
 module.exports = {
-  arrowParens: "always",
+  arrowParens: 'always',
   bracketSpacing: true,
-  endOfLine: "lf",
-  "arrowParens": "always",
-  "bracketSpacing": true,
-  "jsxBracketSameLine": false,
-  "jsxSingleQuote": false,
-  "printWidth": 100,
-  "proseWrap": "always",
-  "quoteProps": "as-needed",
-  "semi": true,
-  "singleQuote": true,
-  "tabWidth": 2,
-  "trailingComma": "all",
-  "useTabs": false
- }
+  endOfLine: 'lf',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false
+ };
