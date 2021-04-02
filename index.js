183c95c23af501ec3a418838e0d035bfa6c345e3
sam <sam@freighttrust.com>
2021-04-02 14:47:53 -0700

    feat(config): release
    Signed-off-by: sam <sam@freighttrust.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// @file config-export
+// @file prettier-config-conformance
 // @license Apache-2.0
 'use strict';
 
 module.exports = {
-  extends: [require.resolve('./core'), require.resolve('./plugins/react')],
+  extends: [require.resolve('./core'), require.resolve('./plugins/react'), require.resolve('prettier.config.js')],
 };
