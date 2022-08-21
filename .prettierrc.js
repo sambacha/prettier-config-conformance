cccd59a9c51faf373366151b84afe71b8fad0f3b
s bacha <sam@manifoldfinance.com>
2022-08-21 07:25:07 -0700

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -14,4 +14,19 @@ module.exports = {
   tabWidth: 2,
   trailingComma: 'all',
   useTabs: false,
+  singleAttributePerLine: true,
+  vueIndentScriptAndStyle: true,
+  overrides: [
+    {
+      files: ['*.css', '*.scss'],
+      parser: [require.resolve('postcss postcss-scss')],
+      options: {
+        /**
+         *  @param printWidth
+         *  @summary printWidth is not a line character capture
+         *  @see {@link https://prettier.io/docs/en/rationale.html#print-width}
+         */
+        printWidth: 100,
+      }
+   ]
 };
